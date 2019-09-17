2639d95b19d0d5cc9bbf2f401aeba82ce9d26c8d
rfguerreros <rfguerreros@unal.edu.co>
2019-09-17 18:01:22 -0500

    Add car mapping

--- car.py
+++ car.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 class Car:
-    def __init_(self,pos,ori,track,world,c_id):
+    def __init__(self,pos,ori,track,world,c_id):
         self.pos = np.asarray(pos)
         self.ori = math.fmod(ori,2*np.pi)
         self.xi = 0
@@ -12,3 +12,13 @@ class Car:
         self.world_in = world
         self.vel = 0
         self.id = c_id
+
+    def sense_world(self,x,y):
+        xi = int((x*(self.world_in.pixels-9)+(4*self.track_in.max_Xcoor-(self.world_in.pixels-5)*self.track_in.min_Xcoor))/self.track_in.lx)
+        yi = int((y*(self.world_in.pixels-9)+(4*self.track_in.max_Ycoor-(self.world_in.pixels-5)*self.track_in.min_Ycoor))/self.track_in.ly)
+        return self.world_in.Space[xi,yi]
+
+    def map(self):
+        xi = int((self.pos[0]*(self.world_in.pixels-9)+(4*self.track_in.max_Xcoor-(self.world_in.pixels-5)*self.track_in.min_Xcoor))/self.track_in.lx)
+        yi = int((self.pos[1]*(self.world_in.pixels-9)+(4*self.track_in.max_Ycoor-(self.world_in.pixels-5)*self.track_in.min_Ycoor))/self.track_in.ly)
+        self.world_in.Space[xi,yi]=self.id
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import math
 import track as t
 import world as w
 import lane as l
+import car as c
 
 pixels = 100
 
@@ -13,6 +14,22 @@ earth = w.World(pixels)
 monza = t.Track(l.lane(min_l))
 
 t.map(monza,earth,pixels)
+
+xmin = monza.min_Xcoor
+xmax = monza.max_Xcoor
+ymin = monza.min_Ycoor
+ymax = monza.max_Ycoor
+
+mecho = c.Car([0.4*(xmax-xmin),0.5*(ymax+ymin)],0,monza,earth,1)
+
+print(xmin)
+print(xmax)
+print(ymin)
+print(ymax)
+
+print(mecho.pos)
+
+mecho.map()
 
 plt.matshow(earth.Space)
 

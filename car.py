4108651029b80caec6ab4d3811affbba138e85ea
rfguerreros <rfguerreros@unal.edu.co>
2019-09-18 18:21:22 -0500

    sensor working

--- car.py
+++ car.py
@@ -1,6 +1,10 @@
 import numpy as np
 import math
 
+def extend_line(x,t,min_l):
+    x[0] = x[0] + 2.*min_l * np.cos(t)
+    x[1] = x[1] + 2.*min_l * np.sin(t)
+
 class Car:
     def __init__(self,pos,ori,track,world,c_id):
         self.pos = np.asarray(pos)
@@ -22,3 +26,84 @@ class Car:
         xi = int((self.pos[0]*(self.world_in.pixels-9)+(4*self.track_in.max_Xcoor-(self.world_in.pixels-5)*self.track_in.min_Xcoor))/self.track_in.lx)
         yi = int((self.pos[1]*(self.world_in.pixels-9)+(4*self.track_in.max_Ycoor-(self.world_in.pixels-5)*self.track_in.min_Ycoor))/self.track_in.ly)
         self.world_in.Space[xi,yi]=self.id
+    
+    def sensor_f(self,min_l):
+        a = 0
+        f = np.zeros(2)
+        f[0] = self.pos[0]
+        f[1] = self.pos[1]
+        touch =  False
+        while touch == False:
+            if self.sense_world(f[0],f[1]) == 0 or self.sense_world(f[0],f[1]) == self.id:
+                a+=2.*min_l
+                extend_line(f,self.ori,min_l)
+            else:
+                 touch = True
+        self.sensor[0] = a
+        return a
+
+    def sensor_rc(self,min_l):
+        a = 0
+        rc =np.zeros(2)
+        rc[0] = self.pos[0]
+        rc[1] = self.pos[1]
+        touch =  False
+        while touch == False:
+            if self.sense_world(rc[0],rc[1]) == 0 or self.sense_world(rc[0],rc[1]) == self.id:
+                a+=2.*min_l
+                extend_line(rc,self.ori-np.pi/4.,min_l)
+            else:
+                 touch = True
+        self.sensor[1] = a
+        return a
+
+    def sensor_lc(self,min_l):
+        a = 0
+        lc = np.zeros(2)
+        lc[0] = self.pos[0]
+        lc[1] = self.pos[1]
+        touch =  False
+        while touch == False:
+            if self.sense_world(lc[0],lc[1]) == 0 or self.sense_world(lc[0],lc[1]) == self.id:
+                a+=2.*min_l
+                extend_line(lc,self.ori+np.pi/4.,min_l)
+            else:
+                 touch = True
+        self.sensor[2] = a
+        return a
+    def sensor_r(self,min_l):
+        a = 0
+        r = np.zeros(2)
+        r[0] = self.pos[0]
+        r[1] = self.pos[1]
+        touch =  False
+        while touch == False:
+            if self.sense_world(r[0],r[1]) == 0 or self.sense_world(r[0],r[1]) == self.id:
+                a+=2.*min_l
+                extend_line(r,self.ori-np.pi/2.,min_l)
+            else:
+                 touch = True
+        self.sensor[3] = a
+        return a
+
+    def sensor_l(self,min_l):
+        a = 0
+        l = np.zeros(2)
+        l[0] = self.pos[0]
+        l[1] = self.pos[1]
+        touch =  False
+        while touch == False:
+            if self.sense_world(l[0],l[1]) == 0 or self.sense_world(l[0],l[1]) == self.id:
+                a+=2.*min_l
+                extend_line(l,self.ori+np.pi/2.,min_l)
+            else:
+                 touch = True
+        self.sensor[4] = a
+        return a
+
+    def sensor_fill(self,min_l):
+        self.sensor_f(min_l)
+        self.sensor_rc(min_l)
+        self.sensor_lc(min_l)
+        self.sensor_r(min_l)
+        self.sensor_l(min_l)
--- main.py
+++ main.py
@@ -6,30 +6,31 @@ import world as w
 import lane as l
 import car as c
 
+print("Loading begins")
+
 pixels = 100
 
-min_l = 0.7
+min_l = 0.01
 
 earth = w.World(pixels)
 monza = t.Track(l.lane(min_l))
 
 t.map(monza,earth,pixels)
 
+print("Loading ends")
+
 xmin = monza.min_Xcoor
 xmax = monza.max_Xcoor
 ymin = monza.min_Ycoor
 ymax = monza.max_Ycoor
 
-mecho = c.Car([0.4*(xmax-xmin),0.5*(ymax+ymin)],0,monza,earth,1)
-
-print(xmin)
-print(xmax)
-print(ymin)
-print(ymax)
-
-print(mecho.pos)
+mecho = c.Car([0.45*(xmax-xmin),0.5*(ymax+ymin)],0,monza,earth,5)
 
 mecho.map()
+
+mecho.sensor_fill(min_l)
+
+print(mecho.sensor)
 
 plt.matshow(earth.Space)
 

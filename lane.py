591f9a2ef5a3f32be70e0c3915032239469d8678
Diego <dvelozad@unal.edu.co>
2019-09-19 15:24:53 -0500

    warning on missing samples added

--- lane.py
+++ lane.py
@@ -1,6 +1,11 @@
 import numpy as np
+import os
 
 def lane(l_min):
     n = np.random.randint(40)
+
+    if not os.path.exists('samples'):
+        print("Missing track samples. Please run track_gen.py")
+
     track = np.load("samples/track_"+str(n)+"_l_min_"+str(l_min)+".npy")
     return track

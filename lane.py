b78f17a882c16c5b3a078851c57a16efd1f22a06
Diego <dvelozad@unal.edu.co>
2019-09-18 19:41:21 -0500

    more samples added

--- lane.py
+++ lane.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def lane(l_min):
-    n = np.random.randint(10)
+    n = np.random.randint(20)
     track = np.load("samples/track_"+str(n)+"_l_min_"+str(l_min)+".npy")
     return track

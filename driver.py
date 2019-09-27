91f695d55e17336775efcbd7a99406070c50066f
rfguerreros <rfguerreros@unal.edu.co>
2019-09-27 11:11:30 -0500

    Added the parameter for the neural networ

--- driver.py
+++ driver.py
@@ -8,6 +8,8 @@ class Driver:
         self.dir = np.zeros(2)
         self.car_in = car
         self.crash = False
+        self.arc = 0
+        self.death = False
 
     def check_crash(self):
         if self.car_in.sense_world(self.car_in.pos[0],self.car_in.pos[1])!=0 and self.car_in.sense_world(self.car_in.pos[0],self.car_in.pos[1])!=self.car_in.id and  self.crash==False:
@@ -16,8 +18,12 @@ class Driver:
     def drive(self,deltaT,min_l):
         self.check_crash()
         if self.crash == False:
+            a0 = np.arctan2(self.car_in.pos[1],self.car_in.pos[0])
             self.car_in.sensor_fill(min_l)
             self.dir = np.matmul(self.car_in.sensor,self.dec)
             self.car_in.move(self.dir,deltaT)
-        if self.crash==True:
+            a1 = np.arctan2(self.car_in.pos[1],self.car_in.pos[0])
+            self.arc += a1-a0
+        if self.crash == True and self.death == False:
             print("Car %d has crashed" %self.car_in.id)
+            self.death = True
--- main.py
+++ main.py
@@ -21,21 +21,3 @@ xmin = monza.min_Xcoor
 xmax = monza.max_Xcoor
 ymin = monza.min_Ycoor
 ymax = monza.max_Ycoor
-
-mecho = c.Car([0.42*(xmax-xmin),0.5*(ymax+ymin)],0,monza,earth,5)
-
-deo = d.Driver(mecho)
-
-mecho.map(mecho.id)
-
-plt.matshow(earth.Space)
-
-t=0
-
-while(deo.crash == False and t<1000):
-    t+=1
-    deo.drive(deltaT,min_l)
-
-plt.matshow(earth.Space)
-
-plt.show()

bafd1c5b79b94930fa3313be96486eae1bca8994
rfguerreros <rfguerreros@unal.edu.co>
2019-09-29 14:22:40 -0500

    random car locator implemented

--- locator.py
+++ locator.py
@@ -1,6 +1,8 @@
 import numpy as np
+import car as c
 
-def locate_cars(world, dire, car):
+
+def locate_cars(dire, car):
     a = np.random.randint(0,100)
     i=1
     foundl = False
@@ -8,86 +10,104 @@ def locate_cars(world, dire, car):
     xi = 0
     xf = 0
     if(dire == 0):
-        while (foundl == False or foundr == False) and i <world.pixels:
-            if world.Space[a,i-1] == 1 and world.Space[a,i] == 0:
+        while (foundl == False or foundr == False) and i <car.world_in.pixels:
+            if car.world_in.Space[a,i-1] == 1 and car.world_in.Space[a,i] == 0:
                 xi = i
                 foundl = True
-            if world.Space[a,i-1] == 0 and world.Space[a,i] == 1 and foundl == True:
+            if car.world_in.Space[a,i-1] == 0 and car.world_in.Space[a,i] == 1 and foundl == True:
                 xf = i
                 foundr=True
             i+=1
         if(xi+20<xf and foundl == True and foundr == True):
             xm = np.random.randint(xi+1,xf-1)
-            if check_surr(a,xm,world) == True:
-                world.Space[a,xm] = 2
+            if check_surr(a,xm,car) == True:
+                find_coordinates(a,xm,car)
+                car.map(car.id)
+                #car.world_in.Space[a,xm] = 2
                 return True
             else:
                 return False
         else:
             return False
     elif (dire == 1):
-        while (foundl == False or foundr == False) and i <world.pixels:
-            if world.Space[i-1,a] == 1 and world.Space[i,a] == 0:
+        while (foundl == False or foundr == False) and i <car.world_in.pixels:
+            if car.world_in.Space[i-1,a] == 1 and car.world_in.Space[i,a] == 0:
                 xi = i
                 foundl = True
-            if world.Space[i-1,a] == 0 and world.Space[i,a] == 1 and foundl == True:
+            if car.world_in.Space[i-1,a] == 0 and car.world_in.Space[i,a] == 1 and foundl == True:
                 xf = i
                 foundr=True
             i+=1
         if(xi+20<xf and foundl == True and foundr == True):
             xm = np.random.randint(xi+1,xf-1)
-            if check_surr(xm,a,world) ==True:
-                world.Space[xm,a] = 2
+            if check_surr(xm,a,car) ==True:
+                find_coordinates(xm,a,car)
+                car.map(car.id)
+                #car.world_in.Space[xm,a] = 2
                 return True
             else:
                 return False
         else:
             return False
     elif (dire == 2):
-        i = world.pixels - 1
+        i = car.world_in.pixels - 1
         while (foundl == False or foundr == False) and i >0:
-            if world.Space[a,i-1] == 0 and world.Space[a,i] == 1:
+            if car.world_in.Space[a,i-1] == 0 and car.world_in.Space[a,i] == 1:
                 xf = i
                 foundl = True
-            if world.Space[a,i-1] == 1 and world.Space[a,i] == 0 and foundl == True:
+            if car.world_in.Space[a,i-1] == 1 and car.world_in.Space[a,i] == 0 and foundl == True:
                 xi = i
                 foundr=True
             i-=1
         if(xi+20<xf and foundl == True and foundr == True):
             xm = np.random.randint(xi+1,xf-1)
-            if check_surr(a,xm,world) == True:
-                world.Space[a,xm] = 2
+            if check_surr(a,xm,car) == True:
+                find_coordinates(a,xm,car)
+                car.map(car.id)
+                #car.world_in.Space[a,xm] = 2
                 return True
             else:
                 return False
         else:
             return False
     elif (dire == 3):
-        i = world.pixels - 1
+        i = car.world_in.pixels - 1
         while (foundl == False or foundr == False) and i >0:
-            if world.Space[i-1,a] == 0 and world.Space[i,a] == 1:
+            if car.world_in.Space[i-1,a] == 0 and car.world_in.Space[i,a] == 1:
                 xf = i
                 foundl = True
-            if world.Space[i-1,a] == 1 and world.Space[i,a] == 0 and foundl == True:
+            if car.world_in.Space[i-1,a] == 1 and car.world_in.Space[i,a] == 0 and foundl == True:
                 xi = i
                 foundr=True
             i-=1
         if(xi+20<xf and foundl == True and foundr == True):
             xm = np.random.randint(xi+1,xf-1)
-            if(check_surr(xm,a,world)==True):
-                world.Space[xm,a] = 2
+            if(check_surr(xm,a,car)==True):
+                #car.world_in.Space[xm,a] = 2
+                find_coordinates(xm,a,car)
+                car.map(car.id)
                 return True
             else:
                 return False
         else:
             return False
 
-def check_surr(a,xm, world):
+def check_surr(a,xm, car):
     b = 0
     for ii in range(a-3,a+4):
         for jj in range(xm-3,xm+4):
-            b+=world.Space[ii,jj]
+            b+=car.world_in.Space[ii,jj]
     if b==0:
         return True
     else:
         return False
+
+def find_coordinates(xi,yi,car):
+    xp = (((xi*car.track_in.lx)-(4.*car.track_in.max_Xcoor-(car.world_in.pixels-5)*car.track_in.min_Xcoor))/(car.world_in.pixels-9.))
+    xa = ((((xi+1)*car.track_in.lx)-(4.*car.track_in.max_Xcoor-(car.world_in.pixels-5)*car.track_in.min_Xcoor))/(car.world_in.pixels-9.))
+    x = (xp+xa)/2.
+    yp = (((yi*car.track_in.ly)-(4.*car.track_in.max_Ycoor-(car.world_in.pixels-5)*car.track_in.min_Ycoor))/(car.world_in.pixels-9.))
+    ya = ((((yi+1)*car.track_in.ly)-(4.*car.track_in.max_Ycoor-(car.world_in.pixels-5)*car.track_in.min_Ycoor))/(car.world_in.pixels-9.))
+    y = (yp+ya)/2.
+    car.pos = np.asarray([x,y])
+
--- main.py
+++ main.py
@@ -9,50 +9,21 @@ import driver as d
 import network as n
 import locator as loc
 
-pixels = 100
-min_l = 0.05
-deltaT = 0.01
-lane_l = 0.6
-
+#############################################################
+################ CONSTANTS FOR PROGRAM ###################### 
+pixels = 100 # CALCULATION SPACE LATERAL SIZE
+min_l = 0.05 # MINIMUM LENGTH FOR DISTANCE MEASUREMENT
+deltaT = 0.01 # TIME INTERVAL
+lane_l = 0.6 # PARAMETER FOR TRACK
 np.random.seed(16)
+#############################################################
+
 
 earth = w.World(pixels)
 monza = t.Track(l.lane(lane_l))
 
 t.map(monza,earth,pixels)
 
-xmin = monza.min_Xcoor
-xmax = monza.max_Xcoor
-ymin = monza.min_Ycoor
-ymax = monza.max_Ycoor
+xmin, xmax, ymin, ymax = monza.min_Xcoor, monza.max_Xcoor, monza.min_Ycoor, monza.max_Ycoor
 
 print("Space is between %d<%d and %d<%d."%(xmin,xmax,ymin,ymax))
-
-i=0
-j=0
-while i<50:
-    l = loc.locate_cars(earth,np.mod(j,4))
-    if(l==True): 
-        i+=1
-    j+=1
-
-plt.matshow(earth.Space)
-
-plt.show()
-
-""" aveo = c.Car([0.42*(xmax-xmin),0.5*(ymax+ymin)],np.pi,monza,earth,5)
-brain = n.Network(2,[6,7,7,2],False)
-deo = d.Driver(aveo,brain)
-
-aveo.map(aveo.id)
-
-t=0
-
-while(deo.crash==False and t<1000):
-    deo.drive(deltaT,min_l)
-    t+=1
-
-cars = []
-brains = []
-drivers = []
-"""
